6dfcf305fdfa2361fe6d30c9df1a4b5ce01ccc46
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-08-14 14:30:20 +0630

    posts and post frontend

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post'
@@ -12,11 +13,16 @@ const Posts = () => {
   console.log(posts);
 
   return (
-   <>
-    <div>Posts</div>
-    <Post/>
-    <Post/>
-   </>
+   !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) =>  (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+
+   )
   )
 }
 
